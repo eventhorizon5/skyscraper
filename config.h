ed9ad7550ab5b7b7fae59976bfd761fbffcb9f62
Ryan Thoryk <ryan@thoryk.com>
2006-06-12 22:49:05 +0000

    Added Unix build environment

---
+++ config.h
@@ -0,0 +1,62 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define when compiling for MacOS/X */
+/* #undef CS_PLATFORM_MACOSX */
+
+/* Define when compiling for Unix and Unix-like (i.e. MacOS/X) */
+#define CS_PLATFORM_UNIX 
+
+/* Define when compiling for Win32 */
+/* #undef CS_PLATFORM_WIN32 */
+
+/* Define to 1 if you have the <elf.h> header file. */
+#define HAVE_ELF_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ryan@tliquest.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "skyscraper"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "skyscraper 1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "skyscraper"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Avoid problem caused by missing <Carbon/CarbonSound.h> */
+/* #undef __CARBONSOUND__ */
