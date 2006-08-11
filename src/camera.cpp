0a88437641175c77db5fc90eb5b7cf0b9ec4c165
Ryan Thoryk <ryan@thoryk.com>
2006-08-11 21:51:30 +0000

    Added RCS ID tags

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,6 +5,8 @@
 	http://www.tliquest.net/skyscraper
 	http://sourceforge.net/projects/skyscraper
 	Contact - ryan@tliquest.net
+	
+	$Id$
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
