dde9cfbd68fd5f59d74ea54e4c3040c2a0b66063
Ryan Thoryk <ryan@thoryk.com>
2008-10-23 04:34:19 +0000

    Updated cut function for origin and CreateDoor call

--- src/stairs.cpp
+++ src/stairs.cpp
@@ -235,7 +235,7 @@ int Stairs::AddDoor(int floor, const char *texture, float thickness, int directi
 {
 	//interface to the SBS AddDoor function
 	
-	return sbs->CreateDoor(StairArray_state[floor - startfloor], texture, thickness, direction, CenterX, CenterZ, width, height, voffset + sbs->GetFloor(floor)->Altitude + sbs->GetFloor(floor)->InterfloorHeight, tw, th);
+	return sbs->CreateDoor(StairArray_state[floor - startfloor], origin, texture, thickness, direction, CenterX, CenterZ, width, height, voffset + sbs->GetFloor(floor)->Altitude + sbs->GetFloor(floor)->InterfloorHeight, tw, th);
 }
 
 void Stairs::CutFloors(csVector2 start, csVector2 end, float startvoffset, float endvoffset)
@@ -268,7 +268,7 @@ void Stairs::CutWall(int floor, csVector3 start, csVector3 end)
 	//the Y values in start and end are both relative to the floor's altitude + interfloor
 
 	float base = sbs->GetFloor(floor)->Altitude + sbs->GetFloor(floor)->InterfloorHeight;
-	sbs->Cut(StairArray_state[floor - startfloor], csVector3(start.x, base + start.y, start.z), csVector3(end.x, base + end.y, end.z), true, false);
+	sbs->Cut(StairArray_state[floor - startfloor], csVector3(start.x, base + start.y, start.z), csVector3(end.x, base + end.y, end.z), true, false, origin);
 }
 
 void Stairs::EnableRange(int floor, int range)
