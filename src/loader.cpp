aa11e219b902c0270ddbc426f57f2ebecc493dac
Ryan Thoryk <ryan@thoryk.com>
2009-01-01 08:13:07 +0000

    Year update

--- src/loader.cpp
+++ src/loader.cpp
@@ -2,7 +2,7 @@
 
 /*
 	Skyscraper 1.2 Alpha - Loader Form
-	Copyright (C)2005-2008 Ryan Thoryk
+	Copyright (C)2005-2009 Ryan Thoryk
 	http://www.skyscrapersim.com
 	http://sourceforge.net/projects/skyscraper
 	Contact - ryan@tliquest.net
