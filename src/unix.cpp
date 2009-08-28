d9219659231d1f9066fea46b810f900798d1957c
Ryan Thoryk <ryan@thoryk.com>
2009-08-28 22:34:59 +0000

    Linux itoa function fix

--- src/unix.cpp
+++ src/unix.cpp
@@ -33,51 +33,9 @@ using namespace std;
 
 const char* itoa(int value, char* str, int radix)
 {
-	int rem = 0;
-	int pos = 0;
-	char ch = '!';
-	do
-	{
-		rem = value % radix;
-		value /= radix;
-		if ( 16 == radix )
-		{
-			if( rem >= 10 && rem <= 15 )
-			{
-				switch( rem )
-				{
-					case 10:
-					ch = 'a';
-					break;
-					case 11:
-					ch ='b';
-					break;
-					case 12:
-					ch = 'c';
-					break;
-					case 13:
-					ch ='d';
-					break;
-					case 14:
-					ch = 'e';
-					break;
-					case 15:
-					ch ='f';
-					break;
-				}
-			}
-		}
-		if( '!' == ch )
-		{
-			str[pos++] = (char) ( rem + 0x30 );
-		}
-		else
-		{
-			str[pos++] = ch ;
-		}
-	}while( value != 0 );
-	str[pos] = '\0';
-	return StrRev(str);
+	//send data to sprintf, discarding the radix value
+	sprintf(str, "%d", value);
+	return str;
 }
 
 //strrev the standard way
