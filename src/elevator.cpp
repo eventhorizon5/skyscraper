06373a83a86c4f2af8b0ca2307ac6a9376866e6a
Ryan Thoryk <ryan@thoryk.com>
2010-05-01 06:25:43 +0000

    Typo fix

--- src/elevator.cpp
+++ src/elevator.cpp
@@ -3507,7 +3507,7 @@ Object* Elevator::AddDoor(const char *open_sound, const char *close_sound, const
 {
 	//interface to the SBS AddDoor function
 
-	if (direction > 8 | direction < 1)
+	if (direction > 8 || direction < 1)
 	{
 		ReportError("Door direction out of range");
 		return 0;
