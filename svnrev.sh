11f4a5de472d423ebf0ed82815ac365e2be19ba6
Ryan Thoryk <ryan@thoryk.com>
2010-11-20 01:12:44 +0000

    Split SBS into shared object on Linux, and restructured source files

--- svnrev.sh
+++ svnrev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-svnrev/svnrev -osrc/revsbs.h -i src/buttonpanel.* src/callbutton.* src/camera.* src/control.* src/directional.* src/door.* src/elevator.* src/elevatordoor.* src/floor.* src/floorindicator.* src/globals.* src/object.* src/sbs.* src/shaft.* src/sound.* src/stairs.* src/unix.* src/wall.*
+svnrev/svnrev -osrc/sbs/revsbs.h -i src/sbs/*.cpp src/sbs/*.h
 
-svnrev/svnrev -osrc/revmain.h -i src/cameracontrol.* src/console.* src/debugpanel.* src/editelevator.* src/fileio.* src/keydialog.* src/meshcontrol.* src/objectinfo.* src/skyscraper.* src/stackwalker.* src/stats.* src/textwindow.* src/uexception.h
+svnrev/svnrev -osrc/frontend/revmain.h -i src/frontend/*.cpp src/frontend/*.h
 
