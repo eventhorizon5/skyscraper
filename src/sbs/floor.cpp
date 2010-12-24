dcff72609b8474e12df29d6eac2bad5b5d0c8fdf
Ryan Thoryk <ryan@thoryk.com>
2010-12-24 19:36:52 +0000

    Fix for object check

--- src/sbs/floor.cpp
+++ src/sbs/floor.cpp
@@ -372,19 +372,25 @@ void Floor::CutAll(const Ogre::Vector3 &start, const Ogre::Vector3 &end, bool cu
 	//cut shafts
 	for (int i = 1; i <= sbs->Shafts(); i++)
 	{
-		if (cutwalls == true)
-			sbs->GetShaft(i)->CutWall(false, Number, start, end);
-		if (cutfloors == true)
-			sbs->GetShaft(i)->CutFloors(false, Ogre::Vector2(start.x, start.z), Ogre::Vector2(end.x, end.z), Altitude, Altitude + FullHeight());
+		if (sbs->GetShaft(i))
+		{
+			if (cutwalls == true)
+				sbs->GetShaft(i)->CutWall(false, Number, start, end);
+			if (cutfloors == true)
+				sbs->GetShaft(i)->CutFloors(false, Ogre::Vector2(start.x, start.z), Ogre::Vector2(end.x, end.z), Altitude, Altitude + FullHeight());
+		}
 	}
 
 	//cut stairs
 	for (int i = 1; i <= sbs->StairsNum(); i++)
 	{
-		if (cutwalls == true)
-			sbs->GetStairs(i)->CutWall(false, Number, start, end);
-		if (cutfloors == true)
-			sbs->GetStairs(i)->CutFloors(false, Ogre::Vector2(start.x, start.z), Ogre::Vector2(end.x, end.z), Altitude, Altitude + FullHeight());
+		if (sbs->GetStairs(i))
+		{
+			if (cutwalls == true)
+				sbs->GetStairs(i)->CutWall(false, Number, start, end);
+			if (cutfloors == true)
+				sbs->GetStairs(i)->CutFloors(false, Ogre::Vector2(start.x, start.z), Ogre::Vector2(end.x, end.z), Altitude, Altitude + FullHeight());
+		}
 	}
 
 	//cut external
