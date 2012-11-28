f23c05e84ef12f29a0062373019e892d0135e5aa
Ryan Thoryk <ryan@thoryk.com>
2012-11-28 05:59:51 +0000

    Added new file copyrights

--- src/frontend/createobject.cpp
+++ src/frontend/createobject.cpp
@@ -1,4 +1,26 @@
-#include "createobject.h"
+/* $Id$ */
+
+/*
+	Skyscraper 1.8 Alpha - Object Creation Dialog
+	Copyright (C)2003-2012 Ryan Thoryk
+	http://www.skyscrapersim.com
+	http://sourceforge.net/projects/skyscraper
+	Contact - ryan@tliquest.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 
 //(*InternalHeaders(CreateObject)
 #include <wx/string.h>
@@ -8,6 +30,7 @@
 #include "globals.h"
 #include "sbs.h"
 #include "parameterviewer.h"
+#include "createobject.h"
 
 extern SBS *Simcore; //external pointer to the SBS engine
 ParameterViewer *viewer;
