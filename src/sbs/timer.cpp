dadd8eb264fb72ae4672e740805a77f32cd8d02e
Ryan Thoryk <ryan@thoryk.com>
2014-01-09 01:19:01 +0000

    Added missing timer class

---
+++ src/sbs/timer.cpp
@@ -0,0 +1,116 @@
+/* $Id$ */
+
+/*
+	Scalable Building Simulator - Timer Class
+	The Skyscraper Project - Version 1.9 Alpha
+	Copyright (C)2004-2014 Ryan Thoryk
+	http://www.skyscrapersim.com
+	http://sourceforge.net/projects/skyscraper
+	Contact - ryan@tliquest.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "globals.h"
+#include "sbs.h"
+#include "unix.h"
+#include "timer.h"
+
+extern SBS *sbs; //external pointer to the SBS engine
+
+TimerObject::TimerObject(Object *parent, const char *name, bool permanent)
+{
+	//set up SBS object
+	object = new Object();
+	object->SetValues(this, parent, "Timer", name, permanent);
+
+	Interval = 0;
+	OneShot = false;
+	LastHit = 0;
+	Running = false;
+	CurrentTime = 0;
+}
+
+TimerObject::~TimerObject()
+{
+	if (Running == true)
+		Stop();
+
+	delete object;
+}
+
+void TimerObject::Start(int milliseconds, bool oneshot)
+{
+	Interval = milliseconds;
+	OneShot = oneshot;
+	Running = true;
+	StartTime = sbs->GetCurrentTime();
+	LastHit = 0;
+	sbs->RegisterTimerCallback(this);
+
+	if (sbs->Verbose)
+		Report("Start");
+}
+
+void TimerObject::Stop()
+{
+	Running = false;
+	sbs->UnregisterTimerCallback(this);
+
+	if (sbs->Verbose)
+		Report("Stop");
+}
+
+bool TimerObject::IsRunning()
+{
+	return Running;
+}
+
+void TimerObject::Check()
+{
+	if (Running == false)
+		return;
+
+	CurrentTime = sbs->GetCurrentTime() - StartTime;
+
+	if (CurrentTime - LastHit >= Interval)
+	{
+		if (sbs->Verbose)
+			Report("Notify");
+
+		Notify();
+		LastHit = CurrentTime;
+
+		if (OneShot == true)
+			Stop();
+	}
+}
+
+void TimerObject::Notify()
+{
+
+}
+
+unsigned long TimerObject::GetCurrentTime()
+{
+	return CurrentTime;
+}
+
+void TimerObject::Report(std::string message)
+{
+	std::string num = ToString2(object->GetNumber());
+	std::string name = object->GetName();
+	sbs->Report("Timer " + name + "(" + num + "): " + message);
+}
---
+++ src/sbs/timer.h
@@ -0,0 +1,54 @@
+/* $Id$ */
+
+/*
+	Scalable Building Simulator - Timer Class
+	The Skyscraper Project - Version 1.9 Alpha
+	Copyright (C)2004-2014 Ryan Thoryk
+	http://www.skyscrapersim.com
+	http://sourceforge.net/projects/skyscraper
+	Contact - ryan@tliquest.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef _SBS_TIMER_H
+#define _SBS_TIMER_H
+
+class SBSIMPEXP TimerObject
+{
+public:
+
+	Object *object; //SBS object
+
+	TimerObject(Object *parent, const char *name, bool permanent);
+	virtual ~TimerObject();
+	void Start(int milliseconds = -1, bool oneshot = false);
+	void Stop();
+	virtual void Notify();
+	bool IsRunning();
+	void Check();
+	unsigned long GetCurrentTime();
+	void Report(std::string message);
+
+private:
+	int Interval;
+	bool OneShot;
+	unsigned long CurrentTime;
+	unsigned long LastHit;
+	unsigned long StartTime;
+	bool Running;
+};
+
+#endif
