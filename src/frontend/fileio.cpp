d720bdbef000eb6aa39dae23812f40176d65f22c
Ryan Thoryk <ryan@thoryk.com>
2015-08-26 18:18:42 +0000

    Added name, id and type variables to the floor section of the script interpreter

--- src/frontend/fileio.cpp
+++ src/frontend/fileio.cpp
@@ -3267,6 +3267,9 @@ int ScriptProcessor::ProcFloors()
 	ReplaceAll(LineData, "%fullheight%", cache_fullheight_s.c_str());
 	ReplaceAll(LineData, "%interfloorheight%", cache_interfloorheight_s.c_str());
 	ReplaceAll(LineData, "%base%", cache_base_s.c_str());
+	ReplaceAll(LineData, "%id%", floor->ID.c_str());
+	ReplaceAll(LineData, "%name%", floor->Name.c_str());
+	ReplaceAll(LineData, "%type%", floor->Type.c_str());
 
 	if (getfloordata == true)
 		return sCheckFloors;
