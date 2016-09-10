0df29dec14f345061e215126998f09e0777e9b09
Ryan Thoryk <ryan@thoryk.com>
2016-09-10 01:01:28 +0000

    AvailableForCall function's car floor range check should include the top and bottom floors

--- src/sbs/elevator.cpp
+++ src/sbs/elevator.cpp
@@ -3223,7 +3223,7 @@ int Elevator::AvailableForCall(int floor, int direction, bool report_on_failure)
 	if (car)
 	{
 		//if direction doesn't go beyond elevator's range
-		if ((direction == 1 && floor < car->GetTopFloor()) || (direction == -1 && floor > car->GetBottomFloor()))
+		if ((direction == 1 && floor <= car->GetTopFloor()) || (direction == -1 && floor >= car->GetBottomFloor()))
 		{
 			//if elevator is running
 			if (IsRunning() == true)
