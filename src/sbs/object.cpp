edbe0e39d53f4e236eaa70f8a11c5c76b8cf21a8
Ryan Thoryk <ryan@thoryk.com>
2017-01-01 21:06:18 +0000

    Include fix

--- src/sbs/object.cpp
+++ src/sbs/object.cpp
@@ -23,7 +23,7 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include <OgreRoot.h>
+#include <OgreLogManager.h>
 #include "globals.h"
 #include "sbs.h"
 #include "scenenode.h"
