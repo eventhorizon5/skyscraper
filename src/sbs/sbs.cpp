33498be93083742a2ba316009c4d6a13da49b8df
Ryan Thoryk <ryan@thoryk.com>
2017-01-05 23:30:59 +0000

    Version update

--- src/sbs/sbs.cpp
+++ src/sbs/sbs.cpp
@@ -63,7 +63,7 @@ SBS::SBS(Ogre::SceneManager* mSceneManager, FMOD::System *fmodsystem, int instan
 	sbs = this;
 	this->mSceneManager = mSceneManager;
 
-	version = "0.10.2." + std::string(SVN_REVSTR);
+	version = "0.10.3." + std::string(SVN_REVSTR);
 	version_state = "Alpha";
 
 	//root object needs to self-register
