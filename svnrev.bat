d842f79df54bfd17b6f02f40400a1150d5100dd2
Ryan Thoryk <ryan@thoryk.com>
2022-09-24 02:10:38 -0500

    Removed svnrev

--- svnrev.bat
+++
@@ -1,3 +0,0 @@
-svnrev\svnrev -osrc\sbs\revsbs.h -i src\sbs\*.cpp src\sbs\*.h
-
-svnrev\svnrev -osrc\frontend\revmain.h -i src\frontend\*.cpp src\frontend\*.h
--- svnrev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-svnrev/svnrev -osrc/sbs/revsbs.h -i src/sbs/*.cpp src/sbs/*.h
-
-svnrev/svnrev -osrc/frontend/revmain.h -i src/frontend/*.cpp src/frontend/*.h
-
