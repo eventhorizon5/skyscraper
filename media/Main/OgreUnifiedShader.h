7ed7ee0f49221c9642e9bcaee7c5cd1a641adfcd
Ryan Thoryk <ryan@thoryk.com>
2022-09-27 12:43:05 -0500

    Fixed resources for GL3+ renderer

--- media/Main/OgreUnifiedShader.h
+++ media/Main/OgreUnifiedShader.h
@@ -6,13 +6,19 @@
 // - shiny: https://ogrecave.github.io/shiny/defining-materials-shaders.html
 // - bgfx: https://github.com/bkaradzic/bgfx/blob/master/src/bgfx_shader.sh
 
-// general usage:
+/// general usage:
 // MAIN_PARAMETERS
 // IN(vec4 vertex, POSITION)
 // MAIN_DECLARATION
 // {
 //     GLSL code here
 // }
+
+/// configuration
+// use macros that will be default with Ogre 14
+// #define USE_OGRE_FROM_FUTURE
+
+// @public-api
 
 #if defined(OGRE_HLSL) || defined(OGRE_CG)
 // HLSL
@@ -23,9 +29,14 @@
 #define mat3 float3x3
 #define mat4 float4x4
 
+#define ivec2 int2
+#define ivec3 int3
+#define ivec4 int4
+
 #define texture1D tex1D
 #define texture2D tex2D
 #define texture3D tex3D
+#define texture2DArray tex2DARRAY
 #define textureCube texCUBE
 #define shadow2D tex2Dcmp
 #define texture2DProj tex2Dproj
@@ -35,6 +46,12 @@ vec4 texture2DLod(sampler2D s, vec2 v, float lod) { return tex2Dlod(s, vec4(v.x,
 #define sampler2DShadow Sampler2DShadow
 
 #define mix lerp
+#define fract frac
+
+float mod(float _a, float _b) { return _a - _b * floor(_a / _b); }
+vec2  mod(vec2  _a, vec2  _b) { return _a - _b * floor(_a / _b); }
+vec3  mod(vec3  _a, vec3  _b) { return _a - _b * floor(_a / _b); }
+vec4  mod(vec4  _a, vec4  _b) { return _a - _b * floor(_a / _b); }
 
 vec2 vec2_splat(float x) { return vec2(x, x); }
 vec3 vec3_splat(float x) { return vec3(x, x, x); }
@@ -57,6 +74,9 @@ mat3 mtxFromCols(vec3 a, vec3 b, vec3 c)
 
 #define STATIC static
 
+#define OGRE_UNIFORMS_BEGIN
+#define OGRE_UNIFORMS_END
+
 #define MAIN_PARAMETERS void main(
 
 #ifdef OGRE_VERTEX_SHADER
@@ -74,55 +94,30 @@ mat3 mtxFromCols(vec3 a, vec3 b, vec3 c)
 #define mat3 metal::float3x3
 #define mat4 metal::float4x4
 
-// fake semantics for attribute locations
-enum {
-    POSITION = 0,
-    BLENDWEIGHT,
-    NORMAL,
-    COLOR0,
-    COLOR = COLOR0,
-    COLOR1,
-    BLENDIDICES = 7,
-    TEXCOORD0,
-    TEXCOORD1,
-    TEXCOORD2,
-    TEXCOORD3,
-    TANGENT = 15
-};
-
 #define IN(decl, sem) decl [[ attribute(sem) ]];
 #else
 // GLSL
-#define SAMPLER1D(name, reg) sampler1D name
-#define SAMPLER2D(name, reg) sampler2D name
-#define SAMPLER3D(name, reg) sampler3D name
-#define SAMPLERCUBE(name, reg) samplerCube name
+#include "GLSL_GL3Support.glsl"
+
+#ifndef USE_OGRE_FROM_FUTURE
+#define _UNIFORM_BINDING(b)
+#elif defined(VULKAN)
+#define _UNIFORM_BINDING(b) layout(binding = b + 2) uniform
+#elif __VERSION__ >= 420
+#define _UNIFORM_BINDING(b) layout(binding = b) uniform
+#else
+#define _UNIFORM_BINDING(b) uniform
+#endif
+
+#define SAMPLER1D(name, reg) _UNIFORM_BINDING(reg) sampler1D name
+#define SAMPLER2D(name, reg) _UNIFORM_BINDING(reg) sampler2D name
+#define SAMPLER3D(name, reg) _UNIFORM_BINDING(reg) sampler3D name
+#define SAMPLER2DARRAY(name, reg) _UNIFORM_BINDING(reg) sampler2DArray name
+#define SAMPLERCUBE(name, reg) _UNIFORM_BINDING(reg) samplerCube name
+#define SAMPLER2DSHADOW(name, reg) _UNIFORM_BINDING(reg) sampler2DShadow name
 
 #define saturate(x) clamp(x, 0.0, 1.0)
 #define mul(a, b) ((a) * (b))
-
-#if __VERSION__ == 100
-mat2 transpose(mat2 m)
-{
-  return mat2(m[0][0], m[1][0],
-              m[0][1], m[1][1]);
-}
-
-mat3 transpose(mat3 m)
-{
-  return mat3(m[0][0], m[1][0], m[2][0],
-              m[0][1], m[1][1], m[2][1],
-              m[0][2], m[1][2], m[2][2]);
-}
-
-mat4 transpose(mat4 m)
-{
-  return mat4(m[0][0], m[1][0], m[2][0], m[3][0],
-              m[0][1], m[1][1], m[2][1], m[3][1],
-              m[0][2], m[1][2], m[2][2], m[3][2],
-              m[0][3], m[1][3], m[2][3], m[3][3]);
-}
-#endif
 
 #define vec2_splat vec2
 #define vec3_splat vec3
@@ -148,11 +143,40 @@ mat3 mtxFromCols(vec3 a, vec3 b, vec3 c)
 #define MAIN_PARAMETERS
 #define MAIN_DECLARATION void main()
 
-#ifdef OGRE_VERTEX_SHADER
-#define IN(decl, sem) attribute decl;
-#define OUT(decl, sem) varying decl;
-#else
-#define IN(decl, sem) varying decl;
-#define OUT(decl, sem) out decl;
 #endif
+
+#if !defined(OGRE_HLSL) && !defined(OGRE_CG)
+// semantics as aliases for attribute locations
+#define POSITION    0
+#define BLENDWEIGHT 1
+#define NORMAL      2
+#define COLOR0      3
+#define COLOR1      4
+#define COLOR COLOR0
+#define FOG         5
+#define BLENDINDICES 7
+#define TEXCOORD0   8
+#define TEXCOORD1   9
+#define TEXCOORD2  10
+#define TEXCOORD3  11
+#define TEXCOORD4  12
+#define TEXCOORD5  13
+#define TEXCOORD6  14
+#define TEXCOORD7  15
+#define TANGENT    14
+#endif
+
+#define OGRE_UNIFORMS(params) OGRE_UNIFORMS_BEGIN params OGRE_UNIFORMS_END
+
+// GL_EXT_shader_explicit_arithmetic_types polyfill
+#ifdef OGRE_GLSLES
+#define float32_t highp float
+#define f32vec2 highp vec2
+#define f32vec3 highp vec3
+#define f32vec4 highp vec4
+#else
+#define float32_t float
+#define f32vec2 vec2
+#define f32vec3 vec3
+#define f32vec4 vec4
 #endif
