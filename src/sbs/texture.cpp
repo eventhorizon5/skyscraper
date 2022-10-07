84ff8d153af350de1db3f40a7eeabda113af1934
Ryan Thoryk <ryan@thoryk.com>
2022-10-07 04:54:42 -0500

    Fix for last commit, should be comparing against texture sizes

--- src/sbs/texture.cpp
+++ src/sbs/texture.cpp
@@ -2273,10 +2273,8 @@ void TextureManager::CopyTexture(Ogre::TexturePtr source, Ogre::TexturePtr desti
 		//if dimensions are the same, use standard copy method to prevent
 		//some crashes on systems with small npot textures
 		//note - this currently doesn't work properly on DirectX, needs fixing
-		if (srcBox.left == dstBox.left &&
-			srcBox.right == dstBox.right &&
-			srcBox.bottom == dstBox.bottom &&
-			srcBox.top == dstBox.top &&
+		if (srcBox.getWidth() == source->getWidth() &&
+			dstBox.getWidth() == destination->getWidth() &&
 			sbs->mRoot->getRenderSystem()->getName() != "Direct3D9 Rendering Subsystem")
 		{
 			source->copyToTexture(destination);
