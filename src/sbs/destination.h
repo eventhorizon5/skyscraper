3eedb81618c1e305734dde431826e0af0bdf713b
Ryan Thoryk <ryan@thoryk.com>
2023-03-19 20:38:56 -0500

    Revert "Added basic Call Button code to Destination object, needs lots of work"
    This reverts commit 21536f2760a979ac84998ee54bb01535af0843b0.

--- src/sbs/destination.h
+++ src/sbs/destination.h
@@ -24,55 +24,21 @@
 #ifndef _SBS_DESTINATION_H
 #define _SBS_DESTINATION_H
 
-#include "lock.h"
-
 namespace SBS {
 
-class SBSIMPEXP Destination : public Object, public Lock
+class SBSIMPEXP Destination : public Object
 {
 public:
 
 	//functions
-	Destination(Object *parent, std::vector<int> &elevators, int floornum, int number);
+	Destination(Object *parent, std::vector<int> &elevators, int floornum);
 	~Destination();
-	void Enabled(bool value);
-	bool Call(int floor);
-	bool ServicesElevator(int elevator);
-	void Loop();
-	void Report(const std::string &message);
-	bool ReportError(const std::string &message);
-	bool FireService(int value);
-	int GetFloor();
-	bool AddElevator(int elevator);
-	bool RemoveElevator(int elevator);
-	bool GetStatus();
-	bool IsEnabled() { return is_enabled; }
-	void ElevatorArrived(int number);
-	int GetElevatorArrived();
-	int FindClosestElevator();
 
 private:
-	void Process();
 
-	int Number; //call button index number (on the specified floor)
-	bool is_enabled;
-	int elevator_arrived;
+	std::vector<int> Elevators; //elevators this controller set is assigned to
 
-	std::vector<int> Elevators; //elevators this call button set is assigned to
-
-	Floor *floor; //floor this call button set is on
-
-	bool Processed;
-	bool Locked;
-	int KeyID;
-	int ActiveElevator; //elevator the call is waiting for
-	bool Status; //is a call active?
-	int DestinationFloor;
-
-	class Timer; //internal timer class
-
-	//timer object
-	Timer *timer;
+	Floor *floor; //floor this controller set is on
 };
 
 }
