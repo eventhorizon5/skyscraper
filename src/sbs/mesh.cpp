9142501ddbdfa250d1ad7885531bb70b63e58d32
Ryan Thoryk <ryan@thoryk.com>
2023-03-28 01:26:33 -0500

    Revert "Fix for last commit, found that a bad resize was causing memory corruption, should be a reserve statement instead"
    This reverts commit 71e09be8eb1ab976057db1ff05bda24af7d826b0.

--- src/sbs/mesh.cpp
+++ src/sbs/mesh.cpp
@@ -895,7 +895,7 @@ bool MeshObject::PolyMesh(const std::string &name, const std::string &material, 
 	//convert to remote positioning
 	if (convert_vertices == true)
 	{
-		converted_vertices.reserve(vertices.size());
+		converted_vertices.resize(vertices.size());
 		for (size_t i = 0; i < vertices.size(); i++)
 		{
 			converted_vertices.push_back(sbs->ToRemote(vertices[i]));
@@ -1074,9 +1074,7 @@ int MeshObject::ProcessSubMesh(std::vector<Geometry> &vertices, std::vector<Tria
 
 		//add vertices
 		for (size_t i = 0; i < vertices.size(); i++)
-		{
 			Submeshes[index].MeshGeometry.push_back(vertices[i]);
-		}
 	}
 	else
 	{
