14a77dfc32398fbed3a4ad22fb5a01d58debb5f1
Ryan Thoryk <ryan@thoryk.com>
2023-03-28 01:47:48 -0500

    Revert "More work, Cut function is now malfunctioning"
    This reverts commit add5798a7cc699ff18fb903f0006c55f0ec71476.

--- src/sbs/texmap.cpp
+++ src/sbs/texmap.cpp
@@ -443,11 +443,14 @@ bool Polygon::IntersectSegment(const Ogre::Vector3 &start, const Ogre::Vector3 &
 	if (!IntersectSegmentPlane(start, end, isect, pr, normal))
 		return false;
 
-	std::vector<Ogre::Vector3> vertices;
+	std::vector<std::vector<Ogre::Vector3> > vertices;
 	GetGeometry(vertices, false, false, false, false, true);
 
-	if (IntersectRay(vertices, start, end))
-		return true;
+	for (size_t i = 0; i < vertices.size(); i++)
+	{
+		if (IntersectRay(vertices[i], start, end))
+			return true;
+	}
 
 	return false;
 }
