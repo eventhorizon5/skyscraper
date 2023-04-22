adbbe8b8770fdcdc02919c8dd8edf3a533aab4e6
Ryan Thoryk <ryan@thoryk.com>
2023-04-22 00:48:23 -0500

    Call buttons are now working under the new system

--- src/sbs/callstation.cpp
+++ src/sbs/callstation.cpp
@@ -610,8 +610,11 @@ bool CallStation::Press(bool up)
 	return result;
 }
 
-bool CallStation::CreateCallButtons(const std::string &sound_file_up, const std::string &sound_file_down, std::string BackTexture, const std::string &UpButtonTexture, const std::string &UpButtonTexture_Lit, const std::string &DownButtonTexture, const std::string &DownButtonTexture_Lit, Real CenterX, Real CenterZ, Real voffset, const std::string &direction, Real BackWidth, Real BackHeight, bool ShowBack, Real tw, Real th)
+bool CallStation::CreateCallButtons(const std::string &sound_file_up, const std::string &sound_file_down, std::string BackTexture, const std::string &UpButtonTexture, const std::string &UpButtonTexture_Lit, const std::string &DownButtonTexture, const std::string &DownButtonTexture_Lit, const std::string &direction, Real BackWidth, Real BackHeight, bool ShowBack, Real tw, Real th)
 {
+	if (!GetController())
+		return false;
+
 	int topfloor = GetController()->GetTopFloor();
 	int bottomfloor = GetController()->GetBottomFloor();
 	bool UpExists = false;
@@ -676,6 +679,7 @@ bool CallStation::CreateCallButtons(const std::string &sound_file_up, const std:
 
 		panel->AddControl(sound_file_down, row, 1, 1, 1, 0, 0, 1, names, textures);
 	}
+
 	return true;
 }
 
--- src/sbs/callstation.h
+++ src/sbs/callstation.h
@@ -72,7 +72,7 @@ public:
 	Control* GetUpControl();
 	Control* GetDownControl();
 	bool Press(bool up);
-	bool CreateCallButtons(const std::string &sound_file_up, const std::string &sound_file_down, std::string BackTexture, const std::string &UpButtonTexture, const std::string &UpButtonTexture_Lit, const std::string &DownButtonTexture, const std::string &DownButtonTexture_Lit, Real CenterX, Real CenterZ, Real voffset, const std::string &direction, Real BackWidth, Real BackHeight, bool ShowBack, Real tw, Real th);
+	bool CreateCallButtons(const std::string &sound_file_up, const std::string &sound_file_down, std::string BackTexture, const std::string &UpButtonTexture, const std::string &UpButtonTexture_Lit, const std::string &DownButtonTexture, const std::string &DownButtonTexture_Lit, const std::string &direction, Real BackWidth, Real BackHeight, bool ShowBack, Real tw, Real th);
 	int GetElevatorArrivedStandard(int floor, bool direction);
 
 private:
