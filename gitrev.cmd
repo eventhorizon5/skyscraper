48fc1ab1acdf1fa781c28e2c0e9efcd345b7b96a
Ryan Thoryk <ryan@thoryk.com>
2023-05-10 17:53:20 -0500

    Enhancements to GitRev script

--- gitrev.cmd
+++ gitrev.cmd
@@ -1,9 +1,11 @@
+echo off
+echo Generating GitRev header...
 erase src\sbs\gitrev.h
-echo #ifndef GITREV_H >> src\sbs\gitrev.h
-echo #define GITREV_H >> src\sbs\gitrev.h
-echo. >> src\sbs\gitrev.h
+echo #ifndef GITREV_H>> src\sbs\gitrev.h
+echo #define GITREV_H>> src\sbs\gitrev.h
+echo.>> src\sbs\gitrev.h
 git rev-list HEAD | find /c /v "" > out
 set /p VV=<out
-echo #define GIT_REV %VV% >> src\sbs\gitrev.h
+echo #define GIT_REV %VV%>> src\sbs\gitrev.h
 erase out
-echo #endif >> src\sbs\gitrev.h
+echo #endif>> src\sbs\gitrev.h
