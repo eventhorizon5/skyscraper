f0f24f9e932b8e728d5ce19859d5fe6a6a5ba803
Ryan Thoryk <ryan@thoryk.com>
2024-03-14 06:19:08 -0500

    Updated email address

--- client/src/skyscraper.h
+++ client/src/skyscraper.h
@@ -3,7 +3,7 @@
 	Copyright (C)2004-2024 Ryan Thoryk
 	https://www.skyscrapersim.net
 	https://sourceforge.net/projects/skyscraper/
-	Contact - ryan@thoryk.com
+	Contact - ryan@skyscrapersim.net
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
