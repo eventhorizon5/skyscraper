86bcfaeb94d49c53550d32a38e978fd6805fa3bd
Ryan Thoryk <ryan@thoryk.com>
2024-05-05 02:24:54 -0500

    Started server code

---
+++ src/server/server.cpp
@@ -0,0 +1,41 @@
+/*
+	Skyscraper 3.0 Alpha - Server Core
+	Copyright (C)2023-2024 Ryan Thoryk
+	https://www.skyscrapersim.net
+	https://sourceforge.net/projects/skyscraper/
+	Contact - ryan@skyscrapersim.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "globals.h"
+#include "skyscraper.h"
+#include "server.h"
+
+namespace Skyscraper {
+
+Server::Server(Skyscraper *frontend)
+{
+	if (!frontend)
+		return;
+
+	this->frontend = frontend;
+}
+
+Server::~Server()
+{
+}
+
+}
