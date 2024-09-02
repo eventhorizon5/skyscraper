8916149bc5ffb7062dc103ae7c34d6ce2ef24ca7
Ryan Thoryk <ryan@thoryk.com>
2024-09-02 01:40:11 -0500

    Fixes

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -8,7 +8,7 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_skyscraper_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -16,7 +16,7 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
 	GDREGISTER_CLASS(Skyscraper);
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_skyscraper_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -24,11 +24,11 @@ void uninitialize_example_module(ModuleInitializationLevel p_level) {
 
 extern "C" {
 // Initialization.
-GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+GDExtensionBool GDE_EXPORT skyscraper_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.register_initializer(initialize_skyscraper_module);
+	init_obj.register_terminator(uninitialize_skyscraper_module);
 	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	return init_obj.init();
--- src/register_types.h
+++ src/register_types.h
@@ -5,8 +5,8 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level);
-void uninitialize_example_module(ModuleInitializationLevel p_level);
+void initialize_skyscraper_module(ModuleInitializationLevel p_level);
+void uninitialize_skyscraper_module(ModuleInitializationLevel p_level);
 
 #endif // SKYSCRAPER_REGISTER_TYPES_H
 
