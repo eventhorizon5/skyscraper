187ead7e4b3b940fa9ba4d5929cc9793a46057de
Ryan Thoryk <ryan@thoryk.com>
2024-09-25 12:57:56 -0500

    Added more copyright info

--- src/openxr/include/OgreOpenXRRenderWindow.h
+++ src/openxr/include/OgreOpenXRRenderWindow.h
@@ -1,3 +1,16 @@
+/*
+    Skyscraper 2.0 - OpenXR Render Window
+    Portions Copyright (C)2024 Ryan Thoryk
+    https://www.skyscrapersim.net
+    https://sourceforge.net/projects/skyscraper/
+    Contact - ryan@skyscrapersim.net
+*/
+
+/*
+    Original work produced by Glastonbridge Software Limited. This code is provided under the MIT license.
+    https://github.com/glastonbridge/OgreOpenXRRenderWindow
+*/
+
 #pragma once
 
 #include "OgreRenderWindow.h"
