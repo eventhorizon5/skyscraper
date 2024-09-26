4c9027ffd4aa3ef747f3006f66998a70d65ac26d
Ryan Thoryk <ryan@thoryk.com>
2024-09-26 06:56:17 -0500

    Started Geometry Controller code

---
+++ src/sbs/geometry.cpp
@@ -0,0 +1,48 @@
+/*
+	Scalable Building Simulator - Geometry Controller
+	The Skyscraper Project - Version 2.1
+	Copyright (C)2004-2024 Ryan Thoryk
+	https://www.skyscrapersim.net
+	https://sourceforge.net/projects/skyscraper/
+	Contact - ryan@skyscrapersim.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <OgreProcedural/Procedural.h>
+#include "globals.h"
+#include "sbs.h"
+#include "geometry.h"
+
+namespace SBS {
+
+GeometryController::GeometryController(Object *parent) : ObjectBase(parent)
+{
+	Ogre::MeshPtr mesh = Procedural::SphereGenerator().setRadius(2.f).setUTile(5.).setVTile(5.).realizeMesh("sphereMesh");
+
+	Ogre::Entity* entity = sbs->mSceneManager->createEntity("sphereMesh");
+	Ogre::SceneNode* node = sbs->mSceneManager->getRootSceneNode()->createChildSceneNode();
+	node->attachObject(entity);
+	node->setPosition(Ogre::Vector3(0, 0, 0));
+	entity->setMaterialName("sphereMesh");
+	entity->setCastShadows(false);
+}
+
+GeometryController::~GeometryController()
+{
+
+}
+
+}
