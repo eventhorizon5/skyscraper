2193e48c9a34aeeba4574d13f599f739b4994156
Ryan Thoryk <ryan@thoryk.com>
2024-10-01 02:58:45 -0500

    Added support for making prims always visible, with the PrimVisible command

--- src/script/commands.cpp
+++ src/script/commands.cpp
@@ -3019,6 +3019,71 @@ int ScriptProcessor::CommandsSection::Run(std::string &LineData)
 		return sNextLine;
 	}
 
+	//PrimTexture command
+	if (StartsWithNoCase(LineData, "primvisible"))
+	{
+		//get data
+		int params = SplitData(LineData, 12);
+
+		if (params != 3)
+			return ScriptError("Incorrect number of parameters");
+
+		std::string name = tempdata[0];
+		TrimString(name);
+		Object *obj = Simcore->GetObject(name);
+
+		if (!obj)
+			return ScriptError("Invalid object " + name);
+
+		Floor *floorobj = 0;
+		Elevator *elevatorobj = 0;
+		ElevatorCar *elevatorcarobj = 0;
+		Shaft::Level *shaftobj = 0;
+		Stairwell::Level *stairsobj = 0;
+		::SBS::SBS *sbs = 0;
+
+		//get parent object
+		if (obj->GetType() == "Floor")
+			floorobj = static_cast<Floor*>(obj);
+		if (obj->GetType() == "Elevator")
+			elevatorobj = static_cast<Elevator*>(obj);
+		if (obj->GetType() == "ElevatorCar")
+			elevatorcarobj = static_cast<ElevatorCar*>(obj);
+		if (obj->GetType() == "Shaft Level")
+			shaftobj = static_cast<Shaft::Level*>(obj);
+		if (obj->GetType() == "Stairwell Level")
+			stairsobj = static_cast<Stairwell::Level*>(obj);
+		if (obj->GetType() == "SBS")
+			sbs = static_cast<::SBS::SBS*>(obj);
+
+		if (elevatorobj)
+			elevatorcarobj = elevatorobj->GetCar(0);
+
+		//stop here if in Check mode
+		if (config->CheckScript == true)
+			return sNextLine;
+
+		//get prim object
+		Primitive *prim = 0;
+		if (floorobj)
+			prim = floorobj->GetPrimitive(tempdata[1]);
+		if (elevatorcarobj)
+			prim = elevatorcarobj->GetPrimitive(tempdata[1]);
+		if (shaftobj)
+			prim = shaftobj->GetPrimitive(tempdata[1]);
+		if (stairsobj)
+			prim = stairsobj->GetPrimitive(tempdata[1]);
+		if (sbs)
+			prim = sbs->GetPrimitive(tempdata[1]);
+
+		if (!prim)
+			return ScriptError("Invalid primitive " + tempdata[1] + " in " + name);
+
+		prim->always_visible = ToBool(tempdata[2]);
+
+		return sNextLine;
+	}
+
 	//Rotate command
 	/*if (StartsWithNoCase(LineData, "rotate "))
 	{
