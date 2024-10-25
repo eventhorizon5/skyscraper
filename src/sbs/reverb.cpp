076ecd325fbf07ed9dfd06ae684768df15fc7045
Ryan Thoryk <ryan@thoryk.com>
2024-10-25 01:29:34 -0500

    Release reverb instances properly on both delete and error

--- src/sbs/reverb.cpp
+++ src/sbs/reverb.cpp
@@ -122,7 +122,7 @@ Reverb::Reverb(Object *parent, const std::string &name, std::string type, const 
 	{
 		std::string fmod_result = FMOD_ErrorString(result);
 		ReportError("Can't set reverb attributes:\n" + fmod_result);
-		//reverb->setProperties(0);
+		reverb->release();
 		reverb = 0;
 		return;
 	}
@@ -135,8 +135,9 @@ Reverb::~Reverb()
 {
 	sbs->DecrementReverbCount();
 
-	//delete reverb instance
-	//reverb->setProperties(0);
+	//release reverb instance
+	if (reverb)
+		reverb->release();
 
 	//unregister from parent
 	if (sbs->FastDelete == false)
