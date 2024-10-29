e442a4db4213e3f12f384d5c3348bc17d865c018
Ryan Thoryk <ryan@thoryk.com>
2024-10-29 11:50:55 -0500

    Version number fixes

--- src/frontend/hal.h
+++ src/frontend/hal.h
@@ -1,5 +1,5 @@
 /*
-	Skyscraper 2.1 - Hardware Abstraction Layer
+	Skyscraper 2.0 - Hardware Abstraction Layer
 	Copyright (C)2004-2024 Ryan Thoryk
 	https://www.skyscrapersim.net
 	https://sourceforge.net/projects/skyscraper/
