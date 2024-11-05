c7d047b13237be9e01b925fc4aba376049e19159
Ryan Thoryk <ryan@thoryk.com>
2024-11-05 07:32:26 -0600

    Only report the dynamic mesh prepares when reporting is on, this fixes an issue where it would be filling up the console in verbose mode.

--- src/sbs/sbs.cpp
+++ src/sbs/sbs.cpp
@@ -3065,7 +3065,7 @@ void SBS::Prepare(bool report)
 		Report("Processing geometry...");
 	for (size_t i = 0; i < dynamic_meshes.size(); i++)
 	{
-		if (sbs->Verbose)
+		if (sbs->Verbose && report == true)
 			Report("DynamicMesh " + ToString((int)i) + " of " + ToString((int)dynamic_meshes.size()));
 		dynamic_meshes[i]->Prepare();
 	}
