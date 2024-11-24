aa4805ebf8ab335d2a94a4710fd33da5174dc284
Ryan Thoryk <ryan@thoryk.com>
2024-11-24 07:27:23 -0600

    If an elevator is stopped, have the elevator relevel and arrive if the current floor's button is pressed

--- src/sbs/elevator.h
+++ src/sbs/elevator.h
@@ -189,7 +189,7 @@ public:
 	int AvailableForCall(bool destination, int floor, int direction, bool report_on_failure = true);
 	bool SelectFloor(int floor);
 	bool Check(Vector3 position);
-	bool ReturnToNearestFloor();
+	bool ReturnToNearestFloor(bool parking = true);
 	bool ReturnToBottomFloor();
 	bool IsLeveled();
 	bool Up();
