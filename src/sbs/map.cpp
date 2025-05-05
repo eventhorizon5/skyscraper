ea410f1b7442ccd44b7a47cd68c7e29aea8be1f0
Ryan Thoryk <ryan@thoryk.com>
2025-05-05 04:34:31 -0500

    Created a map object, that generates a map of the sim engine every 10 seconds when enabled, using an orthographic camera texture

---
+++ src/sbs/map.cpp
@@ -0,0 +1,97 @@
+/*
+	Scalable Building Simulator - Map Object
+	The Skyscraper Project - Version 2.1
+	Copyright (C)2004-2025 Ryan Thoryk
+	https://www.skyscrapersim.net
+	https://sourceforge.net/projects/skyscraper/
+	Contact - ryan@skyscrapersim.net
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "globals.h"
+#include "sbs.h"
+#include "cameratexture.h"
+#include "timer.h"
+#include "map.h"
+
+namespace SBS {
+
+class Map::Timer : public TimerObject
+{
+public:
+	Map *parent;
+	bool shot;
+	Timer(const std::string &name, Map *parent) : TimerObject(parent, name)
+	{
+		this->parent = parent;
+		shot = false;
+	}
+	virtual void Notify();
+};
+
+Map::Map(Object *parent, const std::string &name) : Object(parent)
+{
+	//set up SBS object
+	SetValues("Map", name, false);
+
+	enabled = false;
+
+	//create orthographic cameratexture, used for map generation
+	OrthoCamera = new CameraTexture(this, "OrthoCamera", 3, 0, Vector3(0, 50000, 0), false, Vector3(270, 0, 0));
+	OrthoCamera->EnableOrthographic(true);
+
+	timer = new Timer("Map Timer", this);
+
+	Enabled(true);
+}
+
+Map::~Map()
+{
+	if (OrthoCamera)
+		delete OrthoCamera;
+	OrthoCamera = 0;
+}
+
+void Map::Enabled(bool value)
+{
+	OrthoCamera->Enabled(value);
+	if (value == true)
+		timer->Start(10000, false);
+	else
+		timer->Stop();
+	enabled = value;
+}
+
+bool Map::IsEnabled()
+{
+	return enabled;
+}
+
+void Map::Timer::Notify()
+{
+	if (parent->IsEnabled() == true && shot == false)
+	{
+		parent->Enabled(false);
+		shot = true;
+	}
+	else
+	{
+		parent->Enabled(false);
+		shot = false;
+	}
+}
+
+}
