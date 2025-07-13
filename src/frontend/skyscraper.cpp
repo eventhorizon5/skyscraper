4c9d56c234e503614ac93c0ef156851377d3291c
Ryan Thoryk <ryan@thoryk.com>
2025-07-12 22:44:21 -0500

    Update version to 2.0 RC4

--- src/frontend/skyscraper.cpp
+++ src/frontend/skyscraper.cpp
@@ -180,7 +180,7 @@ bool Skyscraper::OnInit()
 
 	vm->version = "2.0";
 	vm->version_rev = ToString(GIT_REV);
-	vm->version_state = "RC3";
+	vm->version_state = "RC4";
 	vm->version_frontend = vm->version + ".0." + vm->version_rev;
 
 #ifdef USING_WX
