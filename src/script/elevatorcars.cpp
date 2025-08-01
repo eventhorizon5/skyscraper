eventhorizon5/skyscraper

commit 5b7704e817a38a4e8be782dd1328ce87ab63e1c8

Added Rotation parameter for shaft doors, allowing them to be rotated along with elevator cars

src/script/elevatorcars.cpp
@@ -1178,7 +1178,7 @@ int ScriptProcessor::ElevatorCarSection::Run(std::string &LineData)
 		//get data
 		int params = SplitData(LineData, 14);
 
-		if (params < 7 || params > 9)
+		if (params < 7 || params > 10)
 			return ScriptError("Incorrect number of parameters");
 
 		int compat = 0;
@@ -1227,12 +1227,14 @@ int ScriptProcessor::ElevatorCarSection::Run(std::string &LineData)
 			return sNextLine;
 
 		bool result;
-		if (compat == 0)
-			result = car->AddShaftDoors(ToInt(tempdata[0]), tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]));
+		if (compat == 0 && params == 9)
+			result = car->AddShaftDoors(ToInt(tempdata[0]), 0.0, tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]));
+		else if (compat == 0 && params == 10)
+			result = car->AddShaftDoors(ToInt(tempdata[0]), ToFloat(tempdata[9]), tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]));
 		if (compat == 1)
-			result = car->AddShaftDoors(ToInt(tempdata[0]), tempdata[1], tempdata[1], ToFloat(tempdata[2]), ToFloat(tempdata[3]), ToFloat(tempdata[4]), 0, ToFloat(tempdata[5]), ToFloat(tempdata[6]));
+			result = car->AddShaftDoors(ToInt(tempdata[0]), 0.0, tempdata[1], tempdata[1], ToFloat(tempdata[2]), ToFloat(tempdata[3]), ToFloat(tempdata[4]), 0, ToFloat(tempdata[5]), ToFloat(tempdata[6]));
 		if (compat == 2)
-			result = car->AddShaftDoors(ToInt(tempdata[0]), tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), 0, ToFloat(tempdata[6]), ToFloat(tempdata[7]));
+			result = car->AddShaftDoors(ToInt(tempdata[0]), 0.0, tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), 0, ToFloat(tempdata[6]), ToFloat(tempdata[7]));
 
 		if (result == false)
 			return ScriptError();

src/script/floors.cpp
@@ -1308,7 +1308,7 @@ int ScriptProcessor::FloorSection::Run(std::string &LineData)
 		//get data
 		int params = SplitData(LineData, 13);
 
-		if (params != 5 && params != 6 && params != 10)
+		if (params != 5 && params != 6 && params > 11)
 			return ScriptError("Incorrect number of parameters");
 
 		int compat = 0;
@@ -1367,12 +1367,14 @@ int ScriptProcessor::FloorSection::Run(std::string &LineData)
 		Elevator *elev = Simcore->GetElevator(elevator);
 		ElevatorCar *car = elev->GetCar(carnum);
 
-		if (compat == 0)
-			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
+		if (compat == 0 && params == 10)
+			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
+		else if (compat == 0 && params == 11)
+			StoreCommand(car->AddShaftDoor(config->Current, ToFloat(tempdata[10]), ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
 		if (compat == 1)
-			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), tempdata[2], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4])));
+			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4])));
 		if (compat == 2)
-			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5])));
+			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5])));
 		return sNextLine;
 	}
 

src/sbs/objects/elevatorcar.cpp
@@ -1559,7 +1559,7 @@ DoorWrapper* ElevatorCar::AddDoors(int number, const std::string &lefttexture, c
 	return 0;
 }
 
-bool ElevatorCar::AddShaftDoors(int number, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
+bool ElevatorCar::AddShaftDoors(int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
 {
 	//adds shaft's elevator doors specified at a relative central position (off of elevator origin)
 	//uses some parameters (width, height, direction) from AddDoors function
@@ -1567,31 +1567,31 @@ bool ElevatorCar::AddShaftDoors(int number, const std::string &lefttexture, cons
 	if (GetDoor(number))
 	{
 		Report("Adding shaft doors...");
-		return GetDoor(number)->AddShaftDoors(lefttexture, righttexture, thickness, CenterX, CenterZ, voffset, tw, th);
+		return GetDoor(number)->AddShaftDoors(rotation, lefttexture, righttexture, thickness, CenterX, CenterZ, voffset, tw, th);
 	}
 	else
 		ReportError("Invalid door " + ToString(number));
 	return false;
 }
 
-DoorWrapper* ElevatorCar::AddShaftDoor(int floor, int number, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th)
+DoorWrapper* ElevatorCar::AddShaftDoor(int floor, int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th)
 {
 	//adds a single elevator shaft door on the specified floor, with position and thickness parameters first specified
 	//by the SetShaftDoors command.
 
 	if (IsServicedFloor(floor) == true && GetDoor(number))
-		return GetDoor(number)->AddShaftDoor(floor, lefttexture, righttexture, tw, th);
+		return GetDoor(number)->AddShaftDoor(floor, rotation, lefttexture, righttexture, tw, th);
 	else
 		return 0;
 }
 
-DoorWrapper* ElevatorCar::AddShaftDoor(int floor, int number, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
+DoorWrapper* ElevatorCar::AddShaftDoor(int floor, int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
 {
 	//adds a single elevator shaft door on the specified floor, with position and thickness parameters first specified
 	//by the SetShaftDoors command.
 
 	if (IsServicedFloor(floor) == true && GetDoor(number))
-		return GetDoor(number)->AddShaftDoor(floor, lefttexture, righttexture, thickness, CenterX, CenterZ, voffset, tw, th);
+		return GetDoor(number)->AddShaftDoor(floor, rotation, lefttexture, righttexture, thickness, CenterX, CenterZ, voffset, tw, th);
 	else
 		return 0;
 }

src/sbs/objects/elevatorcar.h
@@ -131,9 +131,9 @@ public:
 	void ResetDoors(int number = 0, bool sensor = false);
 	bool DoorsStopped(int number = 0);
 	DoorWrapper* AddDoors(int number, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real width, Real height, bool direction, Real tw, Real th);
-	bool AddShaftDoors(int number, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
-	DoorWrapper* AddShaftDoor(int floor, int number, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th);
-	DoorWrapper* AddShaftDoor(int floor, int number, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
+	bool AddShaftDoors(int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
+	DoorWrapper* AddShaftDoor(int floor, int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th);
+	DoorWrapper* AddShaftDoor(int floor, int number, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
 	void Chime(int number, int floor, bool direction, bool early = false);
 	void EnableDoors(bool value);
 	bool AddFloorSigns(int door_number, bool relative, const std::string &texture_prefix, const std::string &direction, Real CenterX, Real CenterZ, Real width, Real height, Real voffset);

src/sbs/objects/elevatordoor.cpp
@@ -1166,7 +1166,7 @@ bool ElevatorDoor::FinishShaftDoors(bool DoorWalls, bool TrackWalls)
 	return true;
 }
 
-bool ElevatorDoor::AddShaftDoors(const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
+bool ElevatorDoor::AddShaftDoors(Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
 {
 	//adds shaft's elevator doors specified at a relative central position (off of elevator origin)
 	//uses some parameters (width, height, direction) from AddDoor/AddDoors function
@@ -1178,21 +1178,21 @@ bool ElevatorDoor::AddShaftDoors(const std::string &lefttexture, const std::stri
 	//create doors
 	for (size_t i = 0; i < car->ServicedFloors.size(); i++)
 	{
-		if (!AddShaftDoor(car->ServicedFloors[i], lefttexture, righttexture, ShaftDoorThickness, ShaftDoorOrigin.x, ShaftDoorOrigin.z, voffset, tw, th))
+		if (!AddShaftDoor(car->ServicedFloors[i], rotation, lefttexture, righttexture, ShaftDoorThickness, ShaftDoorOrigin.x, ShaftDoorOrigin.z, voffset, tw, th))
 			return false;
 	}
 
 	return true;
 }
 
-DoorWrapper* ElevatorDoor::AddShaftDoor(int floor, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th)
+DoorWrapper* ElevatorDoor::AddShaftDoor(int floor, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th)
 {
 	//compatibility version of AddShaftDoor; please use newer implementation instead
 
-	return AddShaftDoor(floor, lefttexture, righttexture, ShaftDoorThickness, ShaftDoorOrigin.x, ShaftDoorOrigin.z, 0, tw, th);
+	return AddShaftDoor(floor, rotation, lefttexture, righttexture, ShaftDoorThickness, ShaftDoorOrigin.x, ShaftDoorOrigin.z, 0, tw, th);
 }
 
-DoorWrapper* ElevatorDoor::AddShaftDoor(int floor, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
+DoorWrapper* ElevatorDoor::AddShaftDoor(int floor, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th)
 {
 	//adds a single elevator shaft door, with position and thickness parameters first specified
 	//by the SetShaftDoors command.
@@ -1243,7 +1243,7 @@ DoorWrapper* ElevatorDoor::AddShaftDoor(int floor, const std::string &lefttextur
 
 	//finish doors
 	DoorWrapper *wrapper = FinishShaftDoor(floor);
-
+	wrapper->Rotate(Vector3(0, rotation, 0));
 	return wrapper;
 }
 

src/sbs/objects/elevatordoor.h
@@ -72,9 +72,9 @@ public:
 	void Reset(bool sensor = false);
 	bool DoorsStopped();
 	DoorWrapper* AddDoors(const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real width, Real height, bool direction, Real tw, Real th);
-	bool AddShaftDoors(const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
-	DoorWrapper* AddShaftDoor(int floor, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th);
-	DoorWrapper* AddShaftDoor(int floor, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
+	bool AddShaftDoors(Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
+	DoorWrapper* AddShaftDoor(int floor, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real tw, Real th);
+	DoorWrapper* AddShaftDoor(int floor, Real rotation, const std::string &lefttexture, const std::string &righttexture, Real thickness, Real CenterX, Real CenterZ, Real voffset, Real tw, Real th);
 	void Chime(int floor, bool direction);
 	void EarlyChime(int floor, bool direction);
 	bool Loop();

commit b9b0a00f2b9020e3278e8652c8a27524cf008cb5

ScriptProc: fixed AddShaftDoor and AddShaftDoors commands
This fixes a regression from commit 5b7704e817a38a4e8be782dd1328ce87ab63e1c8
in which the shaft door parameters would not be processed correctly

src/script/elevatorcars.cpp
@@ -1229,7 +1229,7 @@ int ScriptProcessor::ElevatorCarSection::Run(std::string &LineData)
 		bool result;
 		if (compat == 0 && params == 9)
 			result = car->AddShaftDoors(ToInt(tempdata[0]), 0.0, tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]));
-		else if (compat == 0 && params == 10)
+		if (compat == 0 && params == 10)
 			result = car->AddShaftDoors(ToInt(tempdata[0]), ToFloat(tempdata[9]), tempdata[1], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]));
 		if (compat == 1)
 			result = car->AddShaftDoors(ToInt(tempdata[0]), 0.0, tempdata[1], tempdata[1], ToFloat(tempdata[2]), ToFloat(tempdata[3]), ToFloat(tempdata[4]), 0, ToFloat(tempdata[5]), ToFloat(tempdata[6]));

src/script/floors.cpp
@@ -1368,13 +1368,15 @@ int ScriptProcessor::FloorSection::Run(std::string &LineData)
 		ElevatorCar *car = elev->GetCar(carnum);
 
 		if (compat == 0 && params == 10)
-			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
-		else if (compat == 0 && params == 11)
-			StoreCommand(car->AddShaftDoor(config->Current, ToFloat(tempdata[10]), ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
+			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), 0.0, tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
+		if (compat == 0 && params == 11)
+			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), ToFloat(tempdata[10]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
+		if (compat == 0)
+			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), 0.0, tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5]), ToFloat(tempdata[6]), ToFloat(tempdata[7]), ToFloat(tempdata[8]), ToFloat(tempdata[9])));
 		if (compat == 1)
-			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4])));
+			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), 0.0, tempdata[2], tempdata[2], ToFloat(tempdata[3]), ToFloat(tempdata[4])));
 		if (compat == 2)
-			StoreCommand(car->AddShaftDoor(config->Current, 0.0, ToInt(tempdata[1]), tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5])));
+			StoreCommand(car->AddShaftDoor(config->Current, ToInt(tempdata[1]), 0.0, tempdata[2], tempdata[3], ToFloat(tempdata[4]), ToFloat(tempdata[5])));
 		return sNextLine;
 	}
 