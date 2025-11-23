eventhorizon5/skyscraper

commit 8caeccfc62e0ef995fb41a5d18b6de28428888f8

Frontend: right-clicking on the "Other Building" button now opens the original (native) file selection dialog

src/frontend/startscreen.cpp
@@ -419,32 +419,43 @@ bool StartScreen::GetMenuInput()
 			Real mouse_x_rel = (w * 2) - 1;
 			Real mouse_y_rel = (h * 2) - 1;
 
-        	//change button status based on mouse position and button press status
-        	if (mouse_x_rel > button->x && mouse_x_rel < button->x + button->size_x && mouse_y_rel > button->y && mouse_y_rel < button->y + button->size_y)
-        	{
-        		if (button->drawn_selected == false && wxGetMouseState().LeftIsDown() == false)
-        		{
-        			if (button->drawn_pressed == true)
-        			{
-        				//user clicked on button
-        				button->drawn_selected = true;
-        				Click(i);
-					return true;
+			//change button status based on mouse position and button press status
+			if (mouse_x_rel > button->x && mouse_x_rel < button->x + button->size_x && mouse_y_rel > button->y && mouse_y_rel < button->y + button->size_y)
+			{
+				if (button->drawn_selected == false && wxGetMouseState().LeftIsDown() == false)
+				{
+					if (button->drawn_pressed == true)
+					{
+						//user clicked on button
+						button->drawn_selected = true;
+						RightClick(i);
+						return true;
         			}
         			button->drawn_selected = true;
         		}
-        		if (button->drawn_pressed == false && wxGetMouseState().LeftIsDown() == true)
-        		{
-        			button->drawn_pressed = true;
-        			button->drawn_selected = false;
-        		}
-        	}
-        	else if (button->drawn_selected == true || button->drawn_pressed == true)
-        	{
-        		button->drawn_selected = false;
-        		button->drawn_pressed = false;
-        	}
-        }
+				if (button->drawn_selected == false && wxGetMouseState().RightIsDown() == false)
+				{
+					if (button->drawn_pressed == true)
+					{
+						//user clicked on button
+						button->drawn_selected = true;
+						Click(i);
+						return true;
+					}
+					button->drawn_selected = true;
+				}
+				if (button->drawn_pressed == false && (wxGetMouseState().LeftIsDown() == true || wxGetMouseState().RightIsDown() == true))
+				{
+					button->drawn_pressed = true;
+					button->drawn_selected = false;
+				}
+			}
+			else if (button->drawn_selected == true || button->drawn_pressed == true)
+			{
+				button->drawn_selected = false;
+				button->drawn_pressed = false;
+			}
+		}
 	}
 #endif
 	return true;
@@ -475,6 +486,28 @@ void StartScreen::Click(int index)
 		//show file selection dialog
 		filename = frontend->SelectBuilding();
 	}
+
+	if (filename != "")
+	{
+		frontend->Load(filename);
+	}
+}
+
+void StartScreen::RightClick(int index)
+{
+	//user clicked a button
+
+	std::string number = ToString(index + 1);
+	std::string filename = "";
+
+	HAL *hal = vm->GetHAL();
+
+	if (index == 0) {}
+	if (index == 1) {}
+	if (index == 2) {}
+	if (index == 3) {}
+	if (index > 3)
+		filename = frontend->SelectBuilding(true);
 
 	if (filename != "")
 	{

src/frontend/startscreen.h
@@ -61,6 +61,7 @@ private:
 
 	bool DrawImage(const std::string &filename, buttondata *button, Real x, Real y, bool center, const std::string &filename_selected = "", const std::string &filename_pressed = "");
 	void Click(int index);
+	void RightClick(int index);
 
 	Skyscraper *frontend;
 	VM *vm;

src/frontend/skyscraper.cpp
@@ -512,9 +512,12 @@ void Skyscraper::StartSound()
 	vm->GetHAL()->PlaySound(filename_full, volume);
 }
 
-std::string Skyscraper::SelectBuilding()
+std::string Skyscraper::SelectBuilding(bool native_dialog)
 {
 #ifdef USING_WX
+
+	if (native_dialog == true)
+		return gui->SelectBuildingNative(vm->data_path);
 
 	if (vm->GetHAL()->GetConfigBool(vm->GetHAL()->configfile, "Skyscraper.Frontend.SelectBuildingNative", false) == false)
 		return gui->SelectBuilding(vm->data_path);

src/frontend/skyscraper.h
@@ -101,7 +101,7 @@ public:
 	void ShowError(const std::string &message);
 	void ShowMessage(const std::string &message);
 	void StartSound();
-	std::string SelectBuilding();
+	std::string SelectBuilding(bool native_dialog = false);
 	bool Load(const std::string &filename, EngineContext *parent = 0, const Vector3 &position = Vector3::ZERO, Real rotation = 0.0, const Vector3 &area_min = Vector3::ZERO, const Vector3 &area_max = Vector3::ZERO);
 	bool Start(EngineContext *engine);
 	void AllowResize(bool value);

commit 57b47c67a0071b42a33438c643dddccfdfec08c6

frontend: Activate menu buttons on mouse-up instead of mouse-down (fixes regression introduced in 8caeccfc)

src/frontend/startscreen.cpp
@@ -422,35 +422,29 @@ bool StartScreen::GetMenuInput()
 			//change button status based on mouse position and button press status
 			if (mouse_x_rel > button->x && mouse_x_rel < button->x + button->size_x && mouse_y_rel > button->y && mouse_y_rel < button->y + button->size_y)
 			{
-				if (button->drawn_selected == false && wxGetMouseState().LeftIsDown() == false)
+				if (button->drawn_pressed == true && wxGetMouseState().LeftIsDown() == false && wxGetMouseState().RightIsDown() == false)
 				{
-					if (button->drawn_pressed == true)
-					{
-						//user clicked on button
-						button->drawn_selected = true;
+					if (right_down_last == true)
 						RightClick(i);
-						return true;
-        			}
-        			button->drawn_selected = true;
-        		}
-				if (button->drawn_selected == false && wxGetMouseState().RightIsDown() == false)
-				{
-					if (button->drawn_pressed == true)
-					{
-						//user clicked on button
-						button->drawn_selected = true;
+					else
 						Click(i);
-						return true;
-					}
+
+					button->drawn_pressed = false;
+					button->drawn_selected = true;
+					return true;
+				}
+				if (wxGetMouseState().LeftIsDown() == true || wxGetMouseState().RightIsDown() == true)
+				{
+					right_down_last = wxGetMouseState().RightIsDown();
+					button->drawn_selected = false;
+					button->drawn_pressed = true;
+				}
+				else
+				{
 					button->drawn_selected = true;
 				}
-				if (button->drawn_pressed == false && (wxGetMouseState().LeftIsDown() == true || wxGetMouseState().RightIsDown() == true))
-				{
-					button->drawn_pressed = true;
-					button->drawn_selected = false;
-				}
 			}
-			else if (button->drawn_selected == true || button->drawn_pressed == true)
+			else
 			{
 				button->drawn_selected = false;
 				button->drawn_pressed = false;

src/frontend/startscreen.h
@@ -54,6 +54,7 @@ private:
 	};
 	buttondata *buttons;
 	int buttoncount;
+	bool right_down_last;
 
 	Ogre::Rectangle2D* background_rect;
 	Ogre::SceneNode* background_node;