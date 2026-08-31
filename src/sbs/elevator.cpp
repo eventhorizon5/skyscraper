718c9a86f2b8fa067b3c64f9340928785b78ea4e
Ryan Thoryk <ryan@thoryk.com>
2026-08-30 21:52:52 -0500

    SBS: addition to last commit - automatically dequeue floor routes when Fire Service Phase 1 is on, when Fire Service Phase 2 is also on.

--- src/sbs/elevator.cpp
+++ src/sbs/elevator.cpp
@@ -1988,6 +1988,10 @@ finish:
 	if (EmergencyStop == 0 && IsManuallyStopped() == false && FireServicePhase1 != 1)
 		DeleteActiveRoute();
 
+	//dequeue floor route in Fire Phase 2 'on' mode
+	if (FireServicePhase1 == 1 && FireServicePhase2 == 1)
+		route_controller->DeleteActiveRoute();
+
 	//reset cars' GotoFloor states
 	for (int i = 1; i <= GetCarCount(); i++)
 	{
