ba0302f607df4ded2fad49b6ba8d328fe2560a45
Ryan Thoryk <ryan@thoryk.com>
2026-09-06 20:55:45 -0500

    SBS: elevator door hold mode should always be enabled while in Fire Service Phase 2 mode, even while on recall floor

--- src/sbs/elevatorcar.cpp
+++ src/sbs/elevatorcar.cpp
@@ -1296,8 +1296,8 @@ bool ElevatorCar::OpenDoors(int number, int whichdoors, int floor, bool manual, 
 	if (sbs->GetPower() == false && manual == false)
 		return false;
 
-	//require open button to be held for fire service phase 2 if not on recall floor
-	if (FirePhase2Active() == 1 && parent->OnRecallFloor() == false && manual == false)
+	//require open button to be held for fire service phase 2
+	if (FirePhase2Active() == 1 && manual == false)
 		hold = true;
 
 	if (parent->Interlocks == true)
